4e92c21edd77d85a28c9f1cf70d5a796dc3a6891
Kraghus <masongage@outlook.com>
2022-05-28 16:12:39 -0400

    Added documentation
    Added documentation to the main function.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// This program asks the user to enter a number and then a larger number.
+// If the second number is not larger, it will swap the numbers to ensure
+// the variables are holding the intended data.
+
 #include "io.h"
 #include "namespace.h"
 #include <iostream>
@@ -5,13 +9,18 @@
 
 int main()
 {
+	// calls the functions to get a number and a larger number from the user.
 	int smallInteger{ small::getInteger() };
 	int largeInteger{ large::getInteger() };
 
+	// prints "Well Done!" to the screen if the user followed the instructions.
 	if (followedInstructions(smallInteger, largeInteger))
 	{
 		printWellDone();
 	}
+
+	// calls a function that swaps the values held by the variables if the user
+	// did not follow the instructions.
 	else
 	{
 		std::cout << "Swapping the values... \n";
